1e35d6b017b8bc57af384a92ed49806a0c1bb6e7
subsub-e <ab13696802@gmail.com>
2025-05-26 11:24:50 +0900

    EDMT-55 feat : msw 로그인 accessToekn  로직 구현

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,17 @@
+let alreadyStarted = false;
+
 export async function initMsw() {
+  // strict 모드로 인해 2번 실행되는거 방지(콘솔창 경고를 없애기 위함)
+  if (alreadyStarted) return;
+  alreadyStarted = true;
+
   if (typeof window === 'undefined') {
     const { server } = await import('./server');
     server.listen();
   } else {
     const { worker } = await import('./browser');
-    await worker.start();
+    await worker.start({
+      onUnhandledRequest: 'warn',
+    });
   }
 }
