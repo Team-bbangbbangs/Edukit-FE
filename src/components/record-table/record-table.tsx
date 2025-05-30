d28cd0de929a01fe46b1855cad8a8ff7eeff713d
subsub-e <ab13696802@gmail.com>
2025-05-31 01:50:29 +0900

    EDMT-87 style : 엑셀로 내보내기 버튼 추가

--- src/components/record-table/record-table.tsx
+++ src/components/record-table/record-table.tsx
@@ -2,6 +2,7 @@
 
 import { useGetRocord } from '@/hooks/api/use-get-record';
 import type { RecordType } from '@/types/api/student-record';
+import { downloadExcel } from '@/util/download-excel';
 
 export default function RecordTable({ recordType }: { recordType: RecordType }) {
   const { data, isPending, isError } = useGetRocord(recordType);
@@ -14,23 +15,31 @@ export default function RecordTable({ recordType }: { recordType: RecordType }) 
   }
 
   return (
-    <table className="w-full border-separate border-spacing-0 rounded-3xl border border-slate-300">
-      <thead className="h-12 [&_th]:border-b [&_th]:border-b-slate-300">
-        <tr>
-          <th className="w-40 pl-5 pr-2 pt-1 text-left align-middle font-bold">학번</th>
-          <th className="w-40 px-2 pt-1 text-left align-middle font-bold">이름</th>
-          <th className="px-2 pt-1 text-left align-middle font-bold">과세특</th>
-        </tr>
-      </thead>
-      <tbody className="[&_td]:border-b [&_td]:border-b-slate-300 [&_tr:last-child_td]:border-b-0">
-        {data?.map((record) => (
-          <tr key={record.id}>
-            <td className="py-2 pl-5 pr-2 align-middle">{record.studentNumber}</td>
-            <td className="p-2 align-middle">{record.name}</td>
-            <td className="p-2 align-middle">{record.content}</td>
+    <div>
+      <table className="w-full border-separate border-spacing-0 rounded-3xl border border-slate-300">
+        <thead className="h-12 [&_th]:border-b [&_th]:border-b-slate-300">
+          <tr>
+            <th className="w-40 pl-5 pr-2 pt-1 text-left align-middle font-bold">학번</th>
+            <th className="w-40 px-2 pt-1 text-left align-middle font-bold">이름</th>
+            <th className="px-2 pt-1 text-left align-middle font-bold">과세특</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="[&_td]:border-b [&_td]:border-b-slate-300 [&_tr:last-child_td]:border-b-0">
+          {data?.map((record) => (
+            <tr key={record.id}>
+              <td className="py-2 pl-5 pr-2 align-middle">{record.studentNumber}</td>
+              <td className="p-2 align-middle">{record.name}</td>
+              <td className="p-2 align-middle">{record.content}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button
+        className="fixed bottom-10 right-10 rounded-full bg-green-600 px-6 py-2 font-bold text-white shadow-xl hover:bg-green-500"
+        onClick={() => downloadExcel(data, recordType)}
+      >
+        엑셀로 내보내기
+      </button>
+    </div>
   );
 }
