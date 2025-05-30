bba20eb9a23025f67b9bd4b9d043e99077e78c21
subsub-e <ab13696802@gmail.com>
2025-05-30 14:42:34 +0900

    EDMT-73 feat : 생활기록부 type 생성

---
+++ src/types/api/student-record.ts
@@ -0,0 +1,8 @@
+export type RecordType = 'subject' | 'behavior' | 'career' | 'free' | 'club';
+
+export type StudentRecord = {
+  id: string;
+  studentNumber: string;
+  name: string;
+  content: string;
+};
