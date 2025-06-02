5fc2c09d6ed2102960b25f9936fc6981f8adb05b
subsub-e <ab13696802@gmail.com>
2025-06-02 23:58:01 +0900

    EDMT-91 refactor : 공통 api 수정에 따른 변경 사항

--- src/services/auth/login.ts
+++ src/services/auth/login.ts
@@ -17,5 +17,10 @@ export const login = async ({ email, password }: LoginProp): Promise<LoginRespon
     throw new Error(error.message || '로그인 실패');
   }
 
+  if (!json.data) {
+    const error = await res.json();
+    throw new Error(error.message || '서버에서 로그인 데이터를 받지 못했습니다.');
+  }
+
   return json.data;
 };
