e96feca254e6c83319ec29607f3235f36ebcc361
subsub-e <ab13696802@gmail.com>
2025-06-03 00:00:32 +0900

    EDMT-91 feat : pagination 컴포넌트 구현

---
+++ src/components/pagination/pagination.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link';
+
+interface PaginationProps {
+  tagId?: string;
+  nowPage?: string;
+  maxPage: number;
+}
+
+const paginationStyle = 'rounded-md min-w-8 h-8 flex items-center justify-center text-center p-1';
+
+export default function Pagination({ tagId, nowPage = '1', maxPage }: PaginationProps) {
+  const currentPage = parseInt(nowPage, 10);
+
+  const blockSize = 5;
+  const startPageNumber = Math.floor((currentPage - 1) / blockSize) * blockSize + 1;
+  const endPageNumber = Math.min(startPageNumber + blockSize - 1, maxPage);
+
+  const pageArr = Array.from(
+    { length: endPageNumber - startPageNumber + 1 },
+    (_, i) => startPageNumber + i,
+  );
+
+  const makeQuery = (page: number) => {
+    const params = new URLSearchParams();
+    params.set('page', page.toString());
+    if (tagId) {
+      params.set('tagId', tagId);
+    }
+    return `?${params.toString()}`;
+  };
+
+  return (
+    <section className="flex items-center justify-center gap-2">
+      {startPageNumber > 5 ? (
+        <Link
+          href={makeQuery(startPageNumber - 1)}
+          className={`${paginationStyle} hover:bg-slate-200`}
+        >
+          {'<'}
+        </Link>
+      ) : (
+        <span className={`${paginationStyle} text-slate-300`}>{'<'}</span>
+      )}
+      {pageArr.map((page) => (
+        <Link
+          key={page}
+          href={makeQuery(page)}
+          className={`${paginationStyle} ${page === currentPage && 'bg-slate-300 font-bold'}`}
+        >
+          {page}
+        </Link>
+      ))}
+      {endPageNumber !== maxPage ? (
+        <Link
+          href={makeQuery(endPageNumber + 1)}
+          className={`${paginationStyle} hover:bg-slate-200`}
+        >
+          {'>'}
+        </Link>
+      ) : (
+        <span className={`${paginationStyle} text-slate-300`}>{'>'}</span>
+      )}
+    </section>
+  );
+}
