82270da1d74127d94b2490e9c3521ba5a3cebea6
subsub-e <ab13696802@gmail.com>
2025-06-03 23:52:50 +0900

    EDMT-114 refactor : 요구사항에 따른 세부 공지사항 타입 추가

--- src/types/api/notice.ts
+++ src/types/api/notice.ts
@@ -1,10 +1,18 @@
 export type NoticeTagType = '공지' | '이벤트';
 
+export type DetailNoticeType = {
+  id: string;
+  tag: NoticeTagType;
+  title: string;
+  content: string;
+  createdAt: string;
+};
+
 export type NoticeType = {
   id: string;
   tag: NoticeTagType;
   title: string;
-  createdAt: Date;
+  createdAt: string;
 };
 
 export type NoticeResponse = {
