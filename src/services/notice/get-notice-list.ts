1c19f520d314e8b281c5ede949885f0bb92810f8
subsub-e <ab13696802@gmail.com>
2025-06-03 23:48:56 +0900

    EDMT-114 refactor : 공지사항 리스트 api url 수정

--- src/services/notice/get-notice-list.ts
+++ src/services/notice/get-notice-list.ts
@@ -12,12 +12,14 @@ export const getNoticeList = async ({ page, tagId }: { page?: string; tagId?: st
 
   const param = new URLSearchParams(query).toString();
 
-  const url = param.length === 0 ? `/api/notice` : `/api/notice?${param}`;
+  const url =
+    param.length === 0
+      ? `http://localhost:9090/api/notice`
+      : `http://localhost:9090/api/notice?${param}`;
 
   const res = await fetch(url, {
     cache: 'no-store',
   });
-  console.log(res);
 
   if (!res.ok) {
     const error = await res.json();
@@ -26,5 +28,5 @@ export const getNoticeList = async ({ page, tagId }: { page?: string; tagId?: st
 
   const json: Response<NoticeResponse> = await res.json();
 
-  return json.data;
+  return json;
 };
