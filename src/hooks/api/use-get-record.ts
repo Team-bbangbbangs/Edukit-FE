7333948d7c561f1c740c5a1bd9aca26ab61e5faf
subsub-e <ab13696802@gmail.com>
2025-06-04 18:31:56 +0900

    EDMT-116 refactor : 나의 학생 관리 api 수정에 따른 커스텀 훅 수정

--- src/hooks/api/use-get-record.ts
+++ src/hooks/api/use-get-record.ts
@@ -1,32 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
-import {
-  getSubjectRecord,
-  getBehaviorRecord,
-  getCareerRecord,
-  getClubRecord,
-  getFreeRecord,
-} from '@/services/student-manage/get-record';
+import { getRecord } from '@/services/student-manage/get-record';
 import type { RecordType, StudentRecord } from '@/types/api/student-record';
 
 export const useGetRocord = (recordtype: RecordType) => {
-  const queryFn = () => {
-    switch (recordtype) {
-      case 'subject':
-        return getSubjectRecord();
-      case 'behavior':
-        return getBehaviorRecord();
-      case 'career':
-        return getCareerRecord();
-      case 'free':
-        return getFreeRecord();
-      case 'club':
-        return getClubRecord();
-    }
-  };
-
   return useQuery<StudentRecord[]>({
-    queryKey: [recordtype],
-    queryFn: queryFn,
+    queryKey: ['record', recordtype],
+    queryFn: () => getRecord(recordtype),
   });
 };
