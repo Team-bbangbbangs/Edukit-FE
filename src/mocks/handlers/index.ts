339ff270512534ed32adf03bafcc3f5574a0f97b
subsub-e <ab13696802@gmail.com>
2025-06-05 18:38:47 +0900

    EDMT-115 feat : 초기 학생 생성하는 mock api 구현

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,6 +1,13 @@
 import { authHandler } from './auth-handler';
+import { createRecords } from './create-record';
 import { getNoticeDetail } from './get-notice-detail';
 import { getNoticeList } from './get-notice-list';
 import { getRecord } from './get-record';
 
-export const handlers = [...authHandler, ...getRecord, ...getNoticeList, ...getNoticeDetail];
+export const handlers = [
+  ...authHandler,
+  ...getRecord,
+  ...getNoticeList,
+  ...getNoticeDetail,
+  ...createRecords,
+];
