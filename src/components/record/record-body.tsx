21b81448a4ad16ca60a14351c9a6fc46739c8bcf
subsub-e <ab13696802@gmail.com>
2025-06-06 18:00:55 +0900

    EDMT-112 refactor : 수정 가능 상태인지 아닌지를 분기 처리하기 위해 record-datail-row 컴포넌트 분리 q ;

--- src/components/record/record-body.tsx
+++ src/components/record/record-body.tsx
@@ -4,6 +4,7 @@ import { useGetRocord } from '@/hooks/api/use-get-record';
 import type { RecordType } from '@/types/api/student-record';
 
 import EmptyRecord from './empty-record';
+import RecordDetailRow from './record-detail-row';
 
 export default function RecordBody({ recordType }: { recordType: RecordType }) {
   const { data, isPending, isError } = useGetRocord(recordType);
@@ -32,11 +33,7 @@ export default function RecordBody({ recordType }: { recordType: RecordType }) {
   return (
     <tbody className="[&_td]:border-b [&_td]:border-b-slate-300 [&_tr:last-child_td]:border-b-0">
       {data?.map((record) => (
-        <tr key={record.id}>
-          <td className="py-2 pl-5 pr-2 align-middle">{record.studentNumber}</td>
-          <td className="p-2 align-middle">{record.name}</td>
-          <td className="p-2 align-middle">{record.content}</td>
-        </tr>
+        <RecordDetailRow key={record.id} record={record} recordType={recordType} />
       ))}
     </tbody>
   );
