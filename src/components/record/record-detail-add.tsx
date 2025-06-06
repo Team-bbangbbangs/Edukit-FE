2ccfa43a7bb9074f98119b5eaeac2b659c7d77c5
subsub-e <ab13696802@gmail.com>
2025-06-07 00:08:44 +0900

    EDMT-75 feat : 생기부 데이터 1개 추가하는 UI 컴포넌트 구현

---
+++ src/components/record/record-detail-add.tsx
@@ -0,0 +1,54 @@
+import { useRef } from 'react';
+
+import { Input } from '@/components/input/input';
+import { useCreateRecordDetail } from '@/hooks/api/use-create-record-detail';
+import type { RecordType } from '@/types/api/student-record';
+
+interface RecordDetailEditProps {
+  recordType: RecordType;
+  onView: () => void;
+}
+
+export default function RecordDetailAdd({ recordType, onView }: RecordDetailEditProps) {
+  const { mutate: createRecordDetail } = useCreateRecordDetail();
+
+  const studentNumberRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLInputElement>(null);
+
+  const handleCreate = () => {
+    const studentNumber = studentNumberRef.current?.value || '';
+    const name = nameRef.current?.value || '';
+    const content = contentRef.current?.value || '';
+
+    createRecordDetail({ recordType, studentNumber, name, content }, { onSuccess: onView });
+  };
+
+  return (
+    <tr className="relative">
+      <td className="py-2 pl-5">
+        <Input ref={studentNumberRef} className="w-full p-1" />
+      </td>
+      <td className="py-2 pl-5">
+        <Input ref={nameRef} className="w-full p-1" />
+      </td>
+      <td className="py-2 pl-5">
+        <Input ref={contentRef} className="w-full p-1" />
+      </td>
+      <td className="absolute right-0 top-14 flex gap-2">
+        <button
+          className="rounded-md bg-slate-800 px-4 pb-1.5 pt-2 text-white hover:bg-slate-950"
+          onClick={onView}
+        >
+          취소하기
+        </button>
+        <button
+          className="rounded-md bg-slate-800 px-4 pb-1.5 pt-2 text-white hover:bg-slate-950"
+          onClick={handleCreate}
+        >
+          추가하기
+        </button>
+      </td>
+    </tr>
+  );
+}
