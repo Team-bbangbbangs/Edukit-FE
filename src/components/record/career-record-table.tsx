17bb0c13f8b7707729033975eca849f7b5354fc0
subsub-e <ab13696802@gmail.com>
2025-06-06 17:53:15 +0900

    EDMT-112 refactor : table style에 round 효과 삭제

--- src/components/record/career-record-table.tsx
+++ src/components/record/career-record-table.tsx
@@ -6,7 +6,7 @@ export default function CareerRecordTable() {
   return (
     <div className="relative">
       <h2 className="text-[26px] font-bold">창의적 체험 활동 - 진로</h2>
-      <table className="mt-4 w-full border-separate border-spacing-0 rounded-3xl border border-slate-300">
+      <table className="mt-4 w-full border-separate border-spacing-0 border border-slate-300">
         <RecordHeader title="창의적 체험 활동 - 진로" />
         <RecordBody recordType="career" />
       </table>
--- src/components/record/subject-record-table.tsx
+++ src/components/record/subject-record-table.tsx
@@ -6,7 +6,7 @@ export default function SubjectRecordTable() {
   return (
     <div className="relative">
       <h2 className="text-[26px] font-bold">세부능력 및 특기사항</h2>
-      <table className="mt-4 w-full border-separate border-spacing-0 rounded-3xl border border-slate-300">
+      <table className="mt-4 w-full border-separate border-spacing-0 border border-slate-300">
         <RecordHeader title="세부능력 및 특기사항" />
         <RecordBody recordType="subject" />
       </table>
