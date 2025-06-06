a1604ef6ed0d276cfe9ba8c431dad01ecadc8786
subsub-e <ab13696802@gmail.com>
2025-06-06 18:00:02 +0900

    EDMT-112 refactor : 생기부 테이블 header style 수정

--- src/components/record/record-header.tsx
+++ src/components/record/record-header.tsx
@@ -1,10 +1,10 @@
 export default function RecordHeader({ title }: { title: string }) {
   return (
-    <thead className="h-12 [&_th]:border-b [&_th]:border-b-slate-300">
+    <thead className="h-12 bg-slate-200 [&_th]:border-b [&_th]:border-b-slate-300">
       <tr>
-        <th className="w-40 pl-5 pr-2 pt-1 text-left align-middle font-bold">학번</th>
-        <th className="w-40 px-2 pt-1 text-left align-middle font-bold">이름</th>
-        <th className="px-2 pt-1 text-left align-middle font-bold">{title}</th>
+        <th className="w-36 pl-5 pt-1 text-left align-middle font-bold">학번</th>
+        <th className="w-32 pl-5 pt-1 text-left align-middle font-bold">이름</th>
+        <th className="pl-5 pt-1 text-left align-middle font-bold">{title}</th>
       </tr>
     </thead>
   );
