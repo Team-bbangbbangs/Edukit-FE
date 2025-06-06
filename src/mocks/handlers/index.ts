050e5e9af563730484b8999ee0ea26befd84dc82
subsub-e <ab13696802@gmail.com>
2025-06-06 17:59:18 +0900

    EDMT-112 feat : 생기부 데이터 삭제 mock delete api 구현 및 handler에 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,8 +1,10 @@
 import { authHandler } from './auth-handler';
 import { createRecords } from './create-record';
+import { deleteRecordDetail } from './delete-record-detail';
 import { getNoticeDetail } from './get-notice-detail';
 import { getNoticeList } from './get-notice-list';
 import { getRecord } from './get-record';
+import { putRecordDetail } from './put-record-detail';
 
 export const handlers = [
   ...authHandler,
@@ -10,4 +12,6 @@ export const handlers = [
   ...getNoticeList,
   ...getNoticeDetail,
   ...createRecords,
+  ...putRecordDetail,
+  ...deleteRecordDetail,
 ];
