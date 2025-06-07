8befdc43e28a3a50f22b9517dd5e1eea7e37b500
subsub-e <ab13696802@gmail.com>
2025-06-08 01:42:28 +0900

    EDMT-128 feat : 생기부 작성 페이지 구현

---
+++ src/app/write-subject/page.tsx
@@ -0,0 +1,5 @@
+import StudentRecordWrite from '@/components/student-record-write/student-record-write';
+
+export default function Page() {
+  return <StudentRecordWrite recordType="subject" />;
+}
