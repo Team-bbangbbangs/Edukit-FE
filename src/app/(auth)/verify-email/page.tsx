3f7b8e590b6864270a1bf9bf4d5e6540fe184337
subsub-e <ab13696802@gmail.com>
2025-06-12 01:11:09 +0900

    EDMT-149 refactor : 이메일 확인 페이지 제목 추가

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,5 +1,10 @@
 import VerifyEmail from '@/components/verify-email/verify-email';
 
 export default function Page() {
-  return <VerifyEmail />;
+  return (
+    <div className="flex flex-col gap-10 text-center">
+      <h1>이메일 인증 확인 페이지</h1>
+      <VerifyEmail />
+    </div>
+  );
 }
