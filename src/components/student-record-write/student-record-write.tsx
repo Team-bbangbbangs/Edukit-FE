b3e019b317ae0ee90af0ce7f57c57c68370a5edb
subsub-e <ab13696802@gmail.com>
2025-06-13 15:02:32 +0900

    EDMT-145 feat : 학생 이름 가져오는 fetch api 연동 및 학생 이름 기반 라우팅 기능 구현

--- src/components/student-record-write/student-record-write.tsx
+++ src/components/student-record-write/student-record-write.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { useEffect } from 'react';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import EmptyRecord from '@/components/record/empty-record';
+import { useGetStudentsName } from '@/hooks/api/use-get-students-name';
 import type { RecordType } from '@/types/api/student-record';
 
 import AiResponse from './ai-response';
@@ -7,12 +13,38 @@ import CharacteristicInput from './characteristic-input';
 import RecordSummary from './record-summary';
 
 export default function StudentRecordWrite({ recordType }: { recordType: RecordType }) {
+  const { data, isPending, isError } = useGetStudentsName(recordType, '2025-1');
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const recordId = searchParams.get('recordId');
+
+  useEffect(() => {
+    if (!recordId && data && data.length > 0) {
+      router.replace(`?recordId=${data[0].recordId}`);
+    }
+  }, [data, recordId, router]);
+
+  if (isError) {
+    return <div>에러입니다.</div>;
+  }
+  if (isPending) {
+    return <div>로딩중입니다.</div>;
+  }
+
+  if (data.length === 0) {
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+        <EmptyRecord recordType={recordType} />
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-10">
-      <CharacteristicInput recordType={recordType} />
+      <CharacteristicInput recordType={recordType} students={data} selectedId={Number(recordId)} />
       <AiResponse recordType={recordType} />
       <hr className="h-[1px] border-0 bg-black" />
-      <RecordSummary recordType={recordType} />
+      <RecordSummary selectedId={Number(recordId)} />
     </div>
   );
 }
