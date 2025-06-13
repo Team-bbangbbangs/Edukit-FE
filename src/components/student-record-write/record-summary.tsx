aa62f5a4964d3af292a1fbb5156d8739a3b840ad
subsub-e <ab13696802@gmail.com>
2025-06-13 15:03:39 +0900

    EDMT-145 feat : 종합 학생 생기부 가져오고, 생성하는 api 연동 및 데이터 연동

--- src/components/student-record-write/record-summary.tsx
+++ src/components/student-record-write/record-summary.tsx
@@ -1,25 +1,60 @@
-import { useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Textarea } from '@/components/textarea/textarea';
-import type { RecordType } from '@/types/api/student-record';
+import { useGetSummaryRecordDetail } from '@/hooks/api/use-get-summary-record-detail';
+import { usePostSummaryRecordDetail } from '@/hooks/api/use-post-summary-record-detail';
 
-export default function RecordSummary({ recordType }: { recordType: RecordType }) {
-  const recordSummaryTextRef = useRef<HTMLTextAreaElement>(null);
+export default function RecordSummary({ selectedId }: { selectedId: number }) {
+  const [description, setDescription] = useState('');
+  const { mutate: postSummaryRecordDetail } = usePostSummaryRecordDetail();
+  const { data, isPending, isError } = useGetSummaryRecordDetail(selectedId);
+
+  useEffect(() => {
+    if (data?.description) {
+      setDescription(data.description);
+    }
+  }, [data?.description]);
+
+  const handleSave = () => {
+    const byteCount = new Blob([description]).size;
+
+    postSummaryRecordDetail({
+      recordId: selectedId,
+      description,
+      byteCount,
+    });
+  };
+
+  if (isError) {
+    return <div>데이터를 불러오지 못했습니다..!</div>;
+  }
+  if (isPending) {
+    return <div>데이터를 불러오는 중입니다..!</div>;
+  }
 
   return (
     <div className="relative flex flex-col gap-4">
       <h4 className="font-bold">종합</h4>
       <Textarea
-        ref={recordSummaryTextRef}
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder="완성본을 적어주세요."
         className="h-60 border-slate-400 p-5 placeholder:text-slate-400"
       />
       <div className="flex justify-end">
-        <button className="justify-end rounded-md bg-slate-800 px-4 pb-1.5 pt-2 text-white hover:bg-slate-950">
+        <button
+          disabled={!description.trim()}
+          className={`rounded-md px-4 pb-1.5 pt-2 text-white ${
+            !description.trim()
+              ? 'cursor-not-allowed bg-slate-400'
+              : 'bg-slate-800 hover:bg-slate-950'
+          }`}
+          onClick={handleSave}
+        >
           저장
         </button>
       </div>
-      <span className="absolute bottom-14 right-2 text-slate-400">1000/1500</span>
+      <span className="absolute bottom-14 right-2 text-slate-400">{`${data.byteCount}/1500`}</span>
     </div>
   );
 }
