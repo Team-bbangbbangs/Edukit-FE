aa023612e95aa3ee62e53249ea367d1e4e89ddd5
subsub-e <ab13696802@gmail.com>
2025-06-13 15:03:12 +0900

    EDMT-145 feat : 받아온 학생 이름을 기반으로 dropdown 컴포넌트 구현 및 routing해주는 기능 구현

--- src/components/student-record-write/characteristic-input.tsx
+++ src/components/student-record-write/characteristic-input.tsx
@@ -1,19 +1,62 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+
+import { useRouter } from 'next/navigation';
 
 import { Textarea } from '@/components/textarea/textarea';
-import type { RecordType } from '@/types/api/student-record';
+import type { RecordType, StudentNameTypes } from '@/types/api/student-record';
 
-export default function CharacteristicInput({ recordType }: { recordType: RecordType }) {
+interface CharacteristicInputProps {
+  recordType: RecordType;
+  students: StudentNameTypes[];
+  selectedId: number;
+}
+
+export default function CharacteristicInput({
+  recordType,
+  students,
+  selectedId,
+}: CharacteristicInputProps) {
   const characteristicInputTextRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
+
+  const selectedStudentName = students.find(
+    (student) => student.recordId === selectedId,
+  )?.studentName;
+
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex justify-between">
         <h2 className="text-[24px] font-bold">학생 특성 기입란</h2>
-        <button className="flex gap-2 rounded-md bg-slate-800 px-6 pb-1.5 pt-2 text-white hover:bg-slate-950">
-          <span>학생 1</span>
-          <span>-</span>
-        </button>
+        <div className="relative">
+          <button
+            onClick={() => setOpen((prev) => !prev)}
+            className="flex gap-2 rounded-md bg-slate-800 px-6 pb-1.5 pt-2 text-white hover:bg-slate-950"
+          >
+            <span>{selectedStudentName}</span>
+            <span>▼</span>
+          </button>
+          {open ? (
+            <div className="absolute top-8 flex max-h-40 w-full flex-col overflow-y-scroll rounded-b-md">
+              {students.map((student) => (
+                <button
+                  key={student.recordId}
+                  onClick={() => {
+                    router.push(`?recordId=${student.recordId}`);
+                    setOpen(false);
+                  }}
+                  disabled={selectedId === student.recordId}
+                  className={`px-2 py-1 text-white ${selectedId === student.recordId ? 'bg-slate-950' : 'bg-slate-600 hover:bg-slate-800'} `}
+                >
+                  {student.studentName}
+                </button>
+              ))}
+            </div>
+          ) : null}
+        </div>
       </div>
+
       <Textarea
         ref={characteristicInputTextRef}
         placeholder="내용을 입력해주세요."
