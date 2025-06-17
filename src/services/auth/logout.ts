639ae0594fb2588cb68eab159974d141d9bc356a
subsub-e <ab13696802@gmail.com>
2025-06-17 21:36:30 +0900

    EDMT-162 feat : logout fetch 함수 구현 및 useMutation으로 wrapping

---
+++ src/services/auth/logout.ts
@@ -0,0 +1,19 @@
+import type { Response } from '@/types/api/response';
+
+export const logout = async (): Promise<Response<void>> => {
+  const res = await fetch('/api/v1/auth/logout', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const json: Response<void> = await res.json();
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message || '로그아웃 실패');
+  }
+
+  return json;
+};
