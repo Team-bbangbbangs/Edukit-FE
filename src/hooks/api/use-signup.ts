428806cddff91820f6c32ae301536f1ba9598502
subsub-e <ab13696802@gmail.com>
2025-06-18 23:14:49 +0900

    EDMT-170 refactor : 회원 가입 후처리 로직 삭제 및 console 제거

--- src/hooks/api/use-signup.ts
+++ src/hooks/api/use-signup.ts
@@ -1,22 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { useRouter } from 'next/navigation';
-
 import { signup } from '@/services/auth/signup';
 import type { SignupTypes } from '@/types/api/auth';
 import type { Response } from '@/types/api/response';
 
 export const useSignup = () => {
-  const router = useRouter();
-
   return useMutation<Response<null>, Error, SignupTypes>({
     mutationFn: signup,
-    onSuccess: (data, variables) => {
+    onSuccess: (data) => {
       console.log(data);
-      router.push(`/verify-email?email=${encodeURIComponent(variables.email)}`);
     },
     onError: (error) => {
-      console.error('회원가입 실패:', error.message);
       alert(error.message);
     },
   });
