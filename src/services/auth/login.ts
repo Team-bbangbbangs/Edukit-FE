602082158cf9f23de66523d8ecc1768420a63a1b
subsub-e <ab13696802@gmail.com>
2025-06-20 00:41:28 +0900

    EDMT-165 refactor : login api 응답 type 수정 및 url 수정

--- src/services/auth/login.ts
+++ src/services/auth/login.ts
@@ -1,25 +1,24 @@
-import type { LoginProp, LoginResponse } from '@/types/api/auth';
+import type { LoginProp, AuthResponse } from '@/types/api/auth';
 import type { Response } from '@/types/api/response';
 
-export const login = async ({ email, password }: LoginProp): Promise<LoginResponse> => {
-  const res = await fetch('/api/login', {
+export const login = async ({ email, password }: LoginProp): Promise<AuthResponse> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    credentials: 'include',
     body: JSON.stringify({ email, password }),
   });
 
-  const json: Response<LoginResponse> = await res.json();
+  const json: Response<AuthResponse> = await res.json();
 
   if (!res.ok) {
-    const error = await res.json();
-    throw new Error(error.message || '로그인 실패');
+    throw new Error(json.message || '로그인 실패');
   }
 
   if (!json.data) {
-    const error = await res.json();
-    throw new Error(error.message || '서버에서 로그인 데이터를 받지 못했습니다.');
+    throw new Error(json.message || '서버에서 로그인 데이터를 받지 못했습니다.');
   }
 
   return json.data;
