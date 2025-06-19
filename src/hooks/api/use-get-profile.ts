7761f1b1be485f695664067fa1a84a1e086e1d86
subsub-e <ab13696802@gmail.com>
2025-06-20 00:47:05 +0900

    EDMT-165 refactor : get-profile api에 accessToken 로직 추가

--- src/hooks/api/use-get-profile.ts
+++ src/hooks/api/use-get-profile.ts
@@ -1,11 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { useAuth } from '@/contexts/auth/use-auth';
 import { getProfile } from '@/services/auth/get-profile';
 import type { UserInfoTypes } from '@/types/api/auth';
 
 export const useGetProfile = () => {
+  const { accessToken } = useAuth();
+
+  if (!accessToken) {
+    throw new Error('로그인 상태가 아닙니다.');
+  }
+
   return useQuery<UserInfoTypes>({
     queryKey: ['profile'],
-    queryFn: () => getProfile(),
+    queryFn: () => getProfile(accessToken),
   });
 };
