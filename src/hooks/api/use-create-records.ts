2920381899c7d4fa28b838d6793dacb4f006edef
subsub-e <ab13696802@gmail.com>
2025-06-21 01:11:16 +0900

    EDMT-180 refactor : use-create-records 성공 시 getRecords 호출하는 로직 추가 및 accessToken로직 추가

--- src/hooks/api/use-create-records.ts
+++ src/hooks/api/use-create-records.ts
@@ -1,14 +1,20 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { useAuth } from '@/contexts/auth/use-auth';
 import { createRecords } from '@/services/student-manage/create-records';
 import type { Response } from '@/types/api/response';
-import type { CreateRecords } from '@/types/api/student-record';
+import type { CreateRecordsInput } from '@/types/api/student-record';
 
 export const useCreateRecords = () => {
-  return useMutation<Response<null>, Error, CreateRecords>({
-    mutationFn: createRecords,
-    onSuccess: (data) => {
-      console.log('생기부 데이터 생성 성공:', data);
+  const { accessToken } = useAuth();
+  const queryClient = useQueryClient();
+
+  return useMutation<Response<null>, Error, CreateRecordsInput>({
+    mutationFn: (params) => createRecords({ ...params, accessToken }),
+    onSuccess: (data: Response<null>, variables: CreateRecordsInput) => {
+      queryClient.invalidateQueries({
+        queryKey: ['records', variables.recordType],
+      });
     },
     onError: (error) => {
       console.error('생기부 데이터 생성 실패:', error.message);
