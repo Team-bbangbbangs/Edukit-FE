fb70405170666368b7c31d594cb67d650bbd27b2
subsub-e <ab13696802@gmail.com>
2025-06-21 01:09:03 +0900

    EDMT-180 refactor : use-get-records 에 accessToken 로직 추가 및 파일 명 수정에 따른 리팩토링

--- src/components/record/excel-button.tsx
+++ src/components/record/excel-button.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import { useGetRocord } from '@/hooks/api/use-get-record';
+import { useGetRecords } from '@/hooks/api/use-get-records';
 import type { RecordType } from '@/types/api/student-record';
 import { downloadExcel } from '@/util/download-excel';
 
 export default function ExcelButton({ recordType }: { recordType: RecordType }) {
-  const { data } = useGetRocord(recordType);
+  const { data } = useGetRecords(recordType);
 
-  if (!data || data.length === 0) return null;
+  if (!data || data.students.length === 0) return null;
 
   return (
     <button
       className="fixed bottom-10 right-10 rounded-full bg-green-600 px-6 py-2 font-bold text-white shadow-xl hover:bg-green-500"
-      onClick={() => downloadExcel(data, recordType)}
+      onClick={() => downloadExcel(data.students, recordType)}
     >
       엑셀로 내보내기
     </button>
---
+++ src/hooks/api/use-get-records.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { useAuth } from '@/contexts/auth/use-auth';
+import { isUnauthorizedError, isNotFoundError } from '@/lib/errors';
+import { getRecords } from '@/services/student-manage/get-records';
+import type { RecordType, StudentsResponse } from '@/types/api/student-record';
+
+export const useGetRecords = (recordType: RecordType) => {
+  const { accessToken } = useAuth();
+
+  const query = useQuery<StudentsResponse>({
+    queryKey: ['records', recordType],
+    queryFn: () => getRecords({ recordType, accessToken, semester: '2025-1' }),
+    retry: 1,
+  });
+
+  return {
+    ...query,
+    isUnauthorized: isUnauthorizedError(query.error),
+    isNotFound: isNotFoundError(query.error),
+  };
+};
