9e874b75f357b253083b1b60fc4f8e73f02102cb
subsub-e <ab13696802@gmail.com>
2025-06-21 01:11:44 +0900

    EDMT-180 refactor : 401, 404 error ui 추가 및 응답값 수정에 따른 리팩토링

--- src/components/record/record-body.tsx
+++ src/components/record/record-body.tsx
@@ -2,7 +2,9 @@
 
 import { useState } from 'react';
 
-import { useGetRocord } from '@/hooks/api/use-get-record';
+import Link from 'next/link';
+
+import { useGetRecords } from '@/hooks/api/use-get-records';
 import type { RecordType } from '@/types/api/student-record';
 
 import EmptyRecord from './empty-record';
@@ -11,7 +13,7 @@ import RecordDetailEdit from './record-detail-edit';
 import RecordDetailView from './record-detail-view';
 
 export default function RecordBody({ recordType }: { recordType: RecordType }) {
-  const { data, isPending, isError } = useGetRocord(recordType);
+  const { data, isPending, isError, isUnauthorized, isNotFound } = useGetRecords(recordType);
   const [editingId, setEditingId] = useState<string | null>(null);
 
   const renderTableRow = (content: React.ReactNode) => (
@@ -25,9 +27,23 @@ export default function RecordBody({ recordType }: { recordType: RecordType }) {
   let content: React.ReactNode;
   if (isPending) {
     content = <p className="text-[20px] font-bold">데이터를 불러오는 중입니다.</p>;
+  } else if (isUnauthorized) {
+    content = (
+      <>
+        <p className="text-[20px] font-bold">로그인이 필요합니다.</p>
+        <Link
+          href="/login"
+          className="rounded-md bg-slate-800 px-4 py-2 text-white hover:bg-slate-950"
+        >
+          로그인
+        </Link>
+      </>
+    );
+  } else if (isNotFound) {
+    content = <EmptyRecord recordType={recordType} />;
   } else if (isError || !data) {
     content = <p className="text-[20px] font-bold">데이터를 불러오는 데 실패했습니다.</p>;
-  } else if (data.length === 0) {
+  } else if (data.students.length === 0) {
     content = <EmptyRecord recordType={recordType} />;
   }
 
@@ -37,19 +53,19 @@ export default function RecordBody({ recordType }: { recordType: RecordType }) {
 
   return (
     <tbody className="[&_td]:border-b [&_td]:border-b-slate-300 [&_tr:last-child_td]:border-b-0">
-      {data?.map((record) =>
-        editingId === record.id ? (
+      {data?.students.map((record) =>
+        editingId === record.recordDetailId ? (
           <RecordDetailEdit
-            key={record.id}
+            key={record.recordDetailId}
             record={record}
             recordType={recordType}
             onView={() => setEditingId(null)}
           />
         ) : (
           <RecordDetailView
-            key={record.id}
+            key={record.recordDetailId}
             record={record}
-            onEdit={() => setEditingId(record.id)}
+            onEdit={() => setEditingId(record.recordDetailId)}
           />
         ),
       )}
