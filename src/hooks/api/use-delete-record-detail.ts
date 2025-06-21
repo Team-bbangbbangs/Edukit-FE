32edd721ef8e8486bdc6d65ca8775a9c4c65bbd6
subsub-e <ab13696802@gmail.com>
2025-06-21 23:44:04 +0900

    EDMT-171 refactor : 생기부 데이터 삭제하는 api 후처리 로직 추가 및 accessToken 로직 추가

--- src/hooks/api/use-delete-record-detail.ts
+++ src/hooks/api/use-delete-record-detail.ts
@@ -1,16 +1,28 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { useAuth } from '@/contexts/auth/use-auth';
 import { deleteRecordDetail } from '@/services/student-manage/delete-record-detail';
 import type { Response } from '@/types/api/response';
+import type { RecordType } from '@/types/api/student-record';
+
+type UseDeleteRecordDetailParams = {
+  recordType: RecordType;
+  recordId: string;
+};
 
 export const useDeleteRecordDetail = () => {
-  return useMutation<Response<null>, Error, string>({
-    mutationFn: deleteRecordDetail,
-    onSuccess: (data) => {
-      console.log('데이터 삭제 성공:', data);
+  const { accessToken } = useAuth();
+  const queryClient = useQueryClient();
+
+  return useMutation<Response<null>, Error, UseDeleteRecordDetailParams>({
+    mutationFn: ({ recordId }) => deleteRecordDetail({ accessToken, recordId }),
+    onSuccess: (data: Response<null>, variables: UseDeleteRecordDetailParams) => {
+      queryClient.invalidateQueries({
+        queryKey: ['records', variables.recordType],
+      });
     },
     onError: (error) => {
-      console.error('데이터 삭제 실패:', error.message);
+      alert(error.message);
     },
   });
 };
