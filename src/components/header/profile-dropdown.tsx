5cc7956239db4ea06d85c20e819fb8192e3e8904
subsub-e <ab13696802@gmail.com>
2025-06-26 21:22:11 +0900

    EDMT-182 refactor : loading 컴포넌트 absolute div 내부로 주입

--- src/components/header/profile-dropdown.tsx
+++ src/components/header/profile-dropdown.tsx
@@ -23,9 +23,6 @@ export function ProfileDropDown({ onClose }: ProfileDropDownProps) {
   const { data, isPending, isError } = useGetProfile();
   const { mutate: handleLogout } = useLogout();
 
-  if (isPending) {
-    return <Loading />;
-  }
   if (isError) {
     return <DefaultError />;
   }
@@ -35,38 +32,49 @@ export function ProfileDropDown({ onClose }: ProfileDropDownProps) {
   const { nickname, isTeacherVerified, school } = data;
 
   return (
-    <div className="absolute right-0 top-[50px] z-10 flex w-[300px] flex-col gap-4 rounded-lg border bg-white p-4 shadow-lg">
-      <div className="flex gap-4">
-        <Link href={'/mypage'} onClick={() => onClose()}>
-          <Image src={ProfileImage} alt={`${nickname}의 프로필 이미지`} width={60} height={60} />
-        </Link>
-        <div className="flex flex-col justify-center">
-          <Link href={'/mypage'} onClick={() => onClose()}>
-            <div className="flex items-center gap-1 font-bold">
-              <Home size={20} />
-              <span className="pt-[2px] text-[20px]">{nickname}</span>
-              <span className="pt-[2px] text-[24px]">{'>'}</span>
-            </div>
-          </Link>
-          {isTeacherVerified ? (
-            <span className="text-sm text-gray-600">{SCHOOL_LABELS[school]} 교사</span>
-          ) : (
+    <div className="absolute right-0 top-[50px] z-10 flex h-[148px] w-[300px] flex-col gap-4 rounded-lg border bg-white p-4 shadow-lg">
+      {isPending ? (
+        <Loading />
+      ) : (
+        <>
+          <div className="flex gap-4">
             <Link href={'/mypage'} onClick={() => onClose()}>
-              <span className="text-sm text-red-500 hover:underline">교사 인증 필요</span>
+              <Image
+                src={ProfileImage}
+                alt={`${nickname}의 프로필 이미지`}
+                width={60}
+                height={60}
+              />
             </Link>
-          )}
-        </div>
-      </div>
-      <div className="flex w-full justify-between">
-        <div className="rounded-sm bg-slate-100 px-10 py-2 text-sm">2025-1학기</div>
-        <button
-          type="button"
-          className="rounded-sm bg-slate-100 px-8 py-2 text-sm hover:bg-slate-200"
-          onClick={() => handleLogout()}
-        >
-          로그아웃
-        </button>
-      </div>
+            <div className="flex flex-col justify-center">
+              <Link href={'/mypage'} onClick={() => onClose()}>
+                <div className="flex items-center gap-1 font-bold">
+                  <Home size={20} />
+                  <span className="pt-[2px] text-[20px]">{nickname}</span>
+                  <span className="pt-[2px] text-[24px]">{'>'}</span>
+                </div>
+              </Link>
+              {isTeacherVerified ? (
+                <span className="text-sm text-gray-600">{SCHOOL_LABELS[school]} 교사</span>
+              ) : (
+                <Link href={'/mypage'} onClick={() => onClose()}>
+                  <span className="text-sm text-red-500 hover:underline">교사 인증 필요</span>
+                </Link>
+              )}
+            </div>
+          </div>
+          <div className="flex w-full justify-between">
+            <div className="rounded-sm bg-slate-100 px-10 py-2 text-sm">2025-1학기</div>
+            <button
+              type="button"
+              className="rounded-sm bg-slate-100 px-8 py-2 text-sm hover:bg-slate-200"
+              onClick={() => handleLogout()}
+            >
+              로그아웃
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
