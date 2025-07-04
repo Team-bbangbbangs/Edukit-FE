044f386eb2acc9a1953b9d546d8607c968e9b6b3
subsub-e <ab13696802@gmail.com>
2025-07-04 17:37:50 +0900

    EDMT-245 refactor : 엔터 적용되게 style 코드 추가

--- src/components/record/record-detail-view.tsx
+++ src/components/record/record-detail-view.tsx
@@ -10,7 +10,7 @@ export default function RecordDetailView({ record, onEdit }: RecordDetailViewPro
     <tr onClick={onEdit} className="h-12 cursor-pointer transition-colors hover:bg-slate-200">
       <td className="py-3 pl-5">{record.studentNumber}</td>
       <td className="py-3 pl-5">{record.studentName}</td>
-      <td className="py-3 pl-5">{record.description}</td>
+      <td className="whitespace-pre-wrap py-3 pl-5">{record.description}</td>
     </tr>
   );
 }
