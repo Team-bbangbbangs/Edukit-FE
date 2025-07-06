c4f3a35055ec969aa5cda69aa30fa19c103e00eb
subsub-e <ab13696802@gmail.com>
2025-07-06 15:41:21 +0900

    EDMT-242 feat : 프로필 수정 api 로직 구현 및 useMutation으로 추상화

---
+++ src/services/auth/patch-profile.ts
@@ -0,0 +1,26 @@
+import type { PatchProfile } from '@/types/api/auth';
+import type { Response } from '@/types/api/response';
+
+export const patchProfile = async ({
+  subject,
+  school,
+  nickname,
+  accessToken,
+}: PatchProfile): Promise<Response<null>> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users/profile`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ subject, school, nickname }),
+  });
+
+  const json: Response<null> = await res.json();
+
+  if (!res.ok) {
+    throw new Error(json.message || '프로필 수정 실패');
+  }
+
+  return json;
+};
