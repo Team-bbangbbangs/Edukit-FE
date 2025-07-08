18be9a6f9146ea82c1f80edbd1b5a3338e455925
subsub-e <ab13696802@gmail.com>
2025-07-08 15:11:49 +0900

    EDMT-221 refactor : 모바일 뷰에서 헤더 레이아웃 깨지는 이슈 수정 및 모바일뷰에서 사이드바 트리거 위치 수정

--- src/app/(record)/layout.tsx
+++ src/app/(record)/layout.tsx
@@ -2,15 +2,13 @@ import Header from '@/components/header/header';
 import { SidebarProvider, SidebarTrigger } from '@/components/sidebar/base-sidebar';
 import MainSidebar from '@/components/sidebar/main-sidebar';
 
-export default function RecordLayout({ children }: { children: React.ReactNode }) {
+export default function RecordLayoutClient({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <SidebarProvider>
+      <SidebarTrigger className="fixed left-1 top-[10px] z-50 md:left-2 md:top-[72px]" />
       <Header />
-      <SidebarProvider>
-        <MainSidebar />
-        <SidebarTrigger className="fixed left-2 top-[72px]" />
-        <main className="h-[calc(100vh-64px)] w-full overflow-y-auto p-10">{children}</main>
-      </SidebarProvider>
-    </>
+      <MainSidebar />
+      <main className={`h-[calc(100vh-64px)] w-full overflow-y-auto p-2 md:p-10`}>{children}</main>
+    </SidebarProvider>
   );
 }
