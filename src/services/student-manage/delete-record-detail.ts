1e93ba0c5c2675b240bf5ba7e39be85014689847
subsub-e <ab13696802@gmail.com>
2025-07-15 13:47:27 +0900

    EDMT-244 refactor : services폴더에서 내 학생 관리 도메인에 해당하는 부분 공통 api fetch로직으로 리팩토링 및 타입 리팩토링

--- src/services/student-manage/delete-record-detail.ts
+++ src/services/student-manage/delete-record-detail.ts
@@ -1,23 +1,6 @@
-import type { Response } from '@/types/api/response';
-import type { DeleteRecordDetailTypes } from '@/types/api/student-record';
+import { api } from '@/lib/api';
+import type { ApiResponseWithoutData } from '@/types/api/response';
 
-export const deleteRecordDetail = async ({
-  recordId,
-  accessToken,
-}: DeleteRecordDetailTypes): Promise<Response<null>> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/student-records/${recordId}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-    },
-  });
-
-  const json: Response<null> = await res.json();
-
-  if (!res.ok) {
-    throw new Error(json.message || '학생 기록 삭제 실패');
-  }
-
-  return json;
+export const deleteRecordDetail = async (recordId: string) => {
+  return api.delete<ApiResponseWithoutData>(`/api/v1/student-records/${recordId}`);
 };
--- src/services/student-manage/get-records.ts
+++ src/services/student-manage/get-records.ts
@@ -1,32 +1,15 @@
-import { ApiError } from '@/lib/errors';
-import type { Response } from '@/types/api/response';
+import { api } from '@/lib/api';
 import type { StudentsResponse, RecordType } from '@/types/api/student-record';
 
 interface GetRecordsProps {
   recordType: RecordType;
-  accessToken: string | null;
   semester: string;
 }
 
-export const getRecords = async ({ recordType, accessToken, semester }: GetRecordsProps) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/student-records/${recordType}?semester=${semester}`,
-    {
-      headers: {
-        ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-      },
+export const getRecords = async ({ recordType, semester }: GetRecordsProps) => {
+  return api.get<StudentsResponse>(`/api/v1/student-records/${recordType}`, {
+    params: {
+      semester,
     },
-  );
-
-  const json: Response<StudentsResponse> = await res.json();
-
-  if (!res.ok) {
-    throw new ApiError(json.status, json.code, json.message || '데이터 fetch 실패');
-  }
-
-  if (!json.data) {
-    throw new Error('서버에서 데이터를 받지 못했습니다.');
-  }
-
-  return json.data;
+  });
 };
