eabd4df747c530fbb67045bc63684195e7d8206a
subsub-e <ab13696802@gmail.com>
2025-07-15 13:46:58 +0900

    EDMT-244 refactor : services폴더에서 notice 도메인에 해당하는 부분 공통 api fetch로직으로 리팩토링 및 타입 리팩토링

--- src/services/notice/delete-admin-notice.ts
+++ src/services/notice/delete-admin-notice.ts
@@ -1,23 +1,6 @@
-import type { DeleteAdminNotice } from '@/types/api/notice';
-import type { Response } from '@/types/api/response';
+import { api } from '@/lib/api';
+import type { ApiResponseWithoutData } from '@/types/api/response';
 
-export const deleteAdminNotice = async ({
-  id,
-  accessToken,
-}: DeleteAdminNotice): Promise<Response<null>> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/notices/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-    },
-  });
-
-  const json: Response<null> = await res.json();
-
-  if (!res.ok) {
-    throw new Error(json.message || '공지사항 삭제 실패');
-  }
-
-  return json;
+export const deleteAdminNotice = async (id: string) => {
+  return api.delete<ApiResponseWithoutData>(`/api/v1/admin/notices/${id}`);
 };
--- src/services/notice/patch-admin-notice.ts
+++ src/services/notice/patch-admin-notice.ts
@@ -1,27 +1,17 @@
-import type { PatchAdminNotice } from '@/types/api/notice';
-import type { Response } from '@/types/api/response';
+import { api } from '@/lib/api';
+import type { ApiResponseWithoutData } from '@/types/api/response';
 
-export const patchAdminNotice = async ({
-  id,
-  categoryId,
-  title,
-  content,
-  accessToken,
-}: PatchAdminNotice): Promise<Response<null>> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/admin/notices/${id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-    },
-    body: JSON.stringify({ categoryId, title, content }),
+interface PatchAdminNotice {
+  id: string;
+  categoryId: number;
+  title: string;
+  content: string;
+}
+
+export const patchAdminNotice = async ({ id, categoryId, title, content }: PatchAdminNotice) => {
+  return api.patch<ApiResponseWithoutData>(`/api/v1/admin/notices/${id}`, {
+    categoryId,
+    title,
+    content,
   });
-
-  const json: Response<null> = await res.json();
-
-  if (!res.ok) {
-    throw new Error(json.message || '공지사항 수정 실패');
-  }
-
-  return json;
 };
