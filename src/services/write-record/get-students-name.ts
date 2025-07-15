6adcca2b389b0fd87f868b913e410685f08531de
subsub-e <ab13696802@gmail.com>
2025-07-15 13:48:01 +0900

    EDMT-244 refactor : services폴더에서 생활기록부 작성 도메인에 해당하는 부분 공통 api fetch로직으로 리팩토링 및 타입 리팩토링

--- src/services/write-record/get-students-name.ts
+++ src/services/write-record/get-students-name.ts
@@ -1,26 +1,10 @@
-import { ApiError } from '@/lib/errors';
-import type { Response } from '@/types/api/response';
-import type { RecordType, StudentNameTypes } from '@/types/api/student-record';
+import { api } from '@/lib/api';
+import type { RecordType, studentDetails } from '@/types/api/student-record';
 
-export const getStudentsName = async (
-  recordType: RecordType,
-  semester: string,
-  accessToken: string | null,
-): Promise<StudentNameTypes[]> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/student-records/${recordType}/students?semester=${semester}`,
-    {
-      headers: {
-        ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
-      },
+export const getStudentsName = async (recordType: RecordType, semester: string) => {
+  return api.get<studentDetails>(`/api/v1/student-records/${recordType}/students`, {
+    params: {
+      semester,
     },
-  );
-
-  const json: Response<{ studentDetails: StudentNameTypes[] }> = await res.json();
-
-  if (!res.ok) {
-    throw new ApiError(json.status, json.code, json.message || '데이터 fetch 실패');
-  }
-
-  return json.data?.studentDetails ?? [];
+  });
 };
