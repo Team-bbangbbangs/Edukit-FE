33b3c437307d437a983aae0fd7e5b8cbb4debda9
subsub-e <ab13696802@gmail.com>
2025-07-17 00:50:53 +0900

    EDMT-259 refactor : msw 환경에서 새로고침 시 mocking이 안되는 이슈가 발생하여 isReady라는 상태값을 추가하고, msw가 완전히 mocking 되고 나서 children 요소를 렌더링하도록 수정

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,4 @@
-let alreadyStarted = false;
-
 export async function initMsw() {
-  if (alreadyStarted) return;
-  alreadyStarted = true;
-
   if (typeof window !== 'undefined') {
     const { worker } = await import('./browser');
     await worker.start({
