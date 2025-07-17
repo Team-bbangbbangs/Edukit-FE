63035d009c0ce0b7be8088780dee2859c0d2ece6
subsub-e <ab13696802@gmail.com>
2025-07-17 19:58:02 +0900

    EDMT-284 refactor : tiptap 에디터 특성 상 <p></p> 가 기본적으로 들어있어 이를 없애주는 정규식 함수로 content가 비어있는지 확인하도록 리팩토링

--- src/components/notice/edit-notice.tsx
+++ src/components/notice/edit-notice.tsx
@@ -33,7 +33,7 @@ export default function EditNotice({ notice }: EditNoticeProps) {
     const title = titleRef.current?.value;
     const content = editorRef.current?.getHTML();
 
-    if (!title?.trim() || !content?.trim()) {
+    if (!title?.trim() || !content?.replace(/<[^>]*>/g, '').trim()) {
       alert('제목과 내용을 모두 입력해주세요.');
       return;
     }
