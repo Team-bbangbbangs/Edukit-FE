9aab92fb834867186ec77ffc9a8b9617aeaa9099
subsub-e <ab13696802@gmail.com>
2025-07-20 20:12:12 +0900

    EDMT-262 refactor : 이메일 인증 에러 코드 추가 및 이메일 인증 되지 않은 유저를 msw 환경에서 처리하기 위한 property 추가

--- src/mocks/utils/check-access-token.ts
+++ src/mocks/utils/check-access-token.ts
@@ -7,10 +7,11 @@ interface MswTokenDataType {
 export function checkAccessToken(authHeader: string | null): {
   isValid: boolean;
   isExpired: boolean;
+  isNotVerified: boolean;
   tokenData?: MswTokenDataType;
 } {
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return { isValid: false, isExpired: false };
+    return { isValid: false, isNotVerified: true, isExpired: false };
   }
 
   const token = authHeader.replace('Bearer ', '');
@@ -18,21 +19,23 @@ export function checkAccessToken(authHeader: string | null): {
   const tokenParts = token.split('.');
 
   if (tokenParts.length !== 2) {
-    return { isValid: false, isExpired: false };
+    return { isValid: false, isNotVerified: true, isExpired: false };
   }
 
   const [tokenType, expiresAtStr] = tokenParts;
   const expiresAt = parseInt(expiresAtStr);
 
   if (isNaN(expiresAt)) {
-    return { isValid: false, isExpired: false };
+    return { isValid: false, isNotVerified: true, isExpired: false };
   }
 
   const now = Date.now();
   const isExpired = now > expiresAt;
+  const isNotVerified = tokenType === 'email-not-verified-user-access-token';
 
   return {
     isValid: true,
+    isNotVerified,
     isExpired,
     tokenData: {
       type: tokenType,
