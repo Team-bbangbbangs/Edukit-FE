94c259b541cc1d4fa539594b053d5485559faff8
subsub-e <ab13696802@gmail.com>
2025-07-27 18:55:15 +0900

    EDMT-303 refactor : 코드 리뷰에 따른 metadata 수정

--- src/app/(main)/(record)/manage-free/page.tsx
+++ src/app/(main)/(record)/manage-free/page.tsx
@@ -1,7 +1,7 @@
 import FreeRecordTable from '@/domains/record/components/student-manage/free-record-table';
 import { createPageMetadata } from '@/shared/constants/metadata';
 
-export const metadata = createPageMetadata('manageStudent');
+export const metadata = createPageMetadata('manageStudent', { url: '/manage-free' });
 
 export default function Page() {
   return <FreeRecordTable />;
--- src/app/(main)/(record)/manage-subject/page.tsx
+++ src/app/(main)/(record)/manage-subject/page.tsx
@@ -1,7 +1,7 @@
 import SubjectRecordTable from '@/domains/record/components/student-manage/subject-record-table';
 import { createPageMetadata } from '@/shared/constants/metadata';
 
-export const metadata = createPageMetadata('manageStudent');
+export const metadata = createPageMetadata('manageStudent', { url: '/manage-subject' });
 
 export default function Page() {
   return <SubjectRecordTable />;
