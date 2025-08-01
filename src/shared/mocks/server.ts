cfc89cc4f7444f2f97af956b16cd31ccad9af62c
subsub-e <ab13696802@gmail.com>
2025-08-02 01:45:45 +0900

    EDMT-341 refactor : test를 위한 mock server 객체 추가

--- src/shared/mocks/server.ts
+++ src/shared/mocks/server.ts
@@ -1,8 +1,10 @@
 import { createMiddleware } from '@mswjs/http-middleware';
 import express from 'express';
+import { setupServer } from 'msw/node';
 
 import { handlers } from '@/shared/mocks/handlers';
 
+// Express 서버 (API 모킹)
 const app = express();
 const port = 9090;
 
@@ -12,3 +14,6 @@ app.use(createMiddleware(...handlers));
 app.listen(port, () => {
   console.log(`http://localhost:${port} => Mock api 서버 오픈!`);
 });
+
+// Jest 테스트용 서버 객체
+export const server = setupServer(...handlers);
