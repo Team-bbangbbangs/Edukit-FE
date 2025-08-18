364982af68c66cf694a4f52db3163bc475530d08
subsub-e <ab13696802@gmail.com>
2025-08-18 23:24:10 +0900

    EDMT-383 refactor : 전체 layout style 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,3 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex h-screen w-full items-center justify-center">{children}</main>;
+  return <main className="flex min-h-screen w-full items-center justify-center">{children}</main>;
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,11 +4,13 @@ import MainSidebar from '@/shared/components/layout/sidebar/main-sidebar';
 
 export default function RecordLayoutClient({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-      <SidebarTrigger className="fixed left-1 top-[10px] z-20 md:left-2 md:top-[72px]" />
-      <Header />
-      <MainSidebar />
-      <main className={`h-[calc(100vh-64px)] w-full overflow-y-auto p-2 md:p-10`}>{children}</main>
-    </SidebarProvider>
+    <main className="min-h-screen overflow-hidden">
+      <SidebarProvider>
+        <SidebarTrigger className="fixed left-1 top-[10px] z-20 md:left-2 md:top-[72px]" />
+        <Header />
+        <MainSidebar />
+        <div className={`h-[calc(100vh-64px)] w-full overflow-y-auto p-2 md:p-10`}>{children}</div>
+      </SidebarProvider>
+    </main>
   );
 }
