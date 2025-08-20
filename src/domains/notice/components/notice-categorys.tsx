1ffb9da5429643bed77d93668c574e6347328641
subsub-e <ab13696802@gmail.com>
2025-08-20 16:45:28 +0900

    EDMT-389 refactor : 상세 공지사항 가져오는 api 명세 수정에 따른 관련 컴포넌트, api, mock api 코드 리팩토링

--- src/domains/notice/components/notice-categorys.tsx
+++ src/domains/notice/components/notice-categorys.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link';
 
+import { type NoticeCategoryType } from '../types/notice';
+
 const baseStyle = 'rounded-full px-5 py-1 pt-1.5 text-center font-bold text-[14px]';
 
 const activeStyle = 'bg-slate-800 text-white border border-white';
 
 const nonActiveStyle = 'bg-white text-black border border-slate-400';
 
-export default function NoticeCategorys({ categoryId }: { categoryId?: string }) {
+export default function NoticeCategorys({ category }: { category?: NoticeCategoryType }) {
   return (
     <div className="flex gap-2 py-4">
-      <Link href="/notice" className={`${baseStyle} ${!categoryId ? activeStyle : nonActiveStyle}`}>
+      <Link href="/notice" className={`${baseStyle} ${!category ? activeStyle : nonActiveStyle}`}>
         전체
       </Link>
       <Link
-        href="/notice?categoryId=2"
-        className={`${baseStyle} ${categoryId === '2' ? activeStyle : nonActiveStyle}`}
+        href="/notice?category=announcement"
+        className={`${baseStyle} ${category === 'announcement' ? activeStyle : nonActiveStyle}`}
       >
         공지
       </Link>
       <Link
-        href="/notice?categoryId=3"
-        className={`${baseStyle} ${categoryId === '3' ? activeStyle : nonActiveStyle}`}
+        href="/notice?category=event"
+        className={`${baseStyle} ${category === 'event' ? activeStyle : nonActiveStyle}`}
       >
         이벤트
       </Link>
