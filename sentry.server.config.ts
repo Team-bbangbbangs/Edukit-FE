798ac838f4b5e5c516b75c4d9cfa3ae538efc6ef
subsub-e <ab13696802@gmail.com>
2025-08-20 16:39:47 +0900

    EDMT-389 chore : sentry 개발 모드에서는 작동 안하게 설정 파일 수정

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,16 +2,19 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://5a894f88a9fbcee243831931c2d46e16@o4509864907571200.ingest.us.sentry.io/4509864915304448",
+  dsn:
+    process.env.NODE_ENV === 'production'
+      ? 'https://5a894f88a9fbcee243831931c2d46e16@o4509864907571200.ingest.us.sentry.io/4509864915304448'
+      : undefined, // 개발 환경에서는 비활성화
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
 
   // Enable logs to be sent to Sentry
-  enableLogs: true,
+  enableLogs: process.env.NODE_ENV === 'production',
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,23 +5,24 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://5a894f88a9fbcee243831931c2d46e16@o4509864907571200.ingest.us.sentry.io/4509864915304448',
+  dsn:
+    process.env.NODE_ENV === 'production'
+      ? 'https://5a894f88a9fbcee243831931c2d46e16@o4509864907571200.ingest.us.sentry.io/4509864915304448'
+      : undefined, // 개발 환경에서는 비활성화
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: process.env.NODE_ENV === 'production' ? 1 : 0,
   // Enable logs to be sent to Sentry
-  enableLogs: true,
+  enableLogs: process.env.NODE_ENV === 'production',
 
   // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 0,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: process.env.NODE_ENV === 'production' ? 1.0 : 0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
