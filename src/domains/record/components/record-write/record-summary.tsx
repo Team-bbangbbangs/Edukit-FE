5292d462fdb8650820634dc1117f1b5fb2077577
subsub-e <ab13696802@gmail.com>
2025-09-07 22:27:12 +0900

    EDMT-414 refactor : 생활기록부 가져오는 api 및 수정하는 api 연동 및 UI 수정

--- src/domains/record/components/record-write/record-summary.tsx
+++ src/domains/record/components/record-write/record-summary.tsx
@@ -2,28 +2,28 @@ import { useState, useEffect } from 'react';
 
 import { useRouter } from 'next/navigation';
 
-import { usePostSummaryRecordDetail } from '@/domains/record/apis/mutations/use-post-summary-record-detail';
-import { useGetSummaryRecordDetail } from '@/domains/record/apis/queries/use-get-summary-record-detail';
+import { usePostRecordDetail } from '@/domains/record/apis/mutations/use-post-record-detail';
+import { useGetRecordDetail } from '@/domains/record/apis/queries/use-get-record-detail';
 import SaveSummaryRecordModal from '@/domains/record/components/record-write/save-summary-record-modal';
 import type { RecordType } from '@/domains/record/types/record';
 import { calculateByte } from '@/domains/record/utils/calculate-byte';
+import Button from '@/shared/components/ui/button/button';
 import Loading from '@/shared/components/ui/loading/loading';
-import { Textarea } from '@/shared/components/ui/textarea/textarea';
 import { useAutoResizeTextarea } from '@/shared/hooks/use-auto-resize-textarea';
 
 interface RecordSummaryProps {
   selectedId: number;
   recordType: RecordType;
+  bytesLimit: number;
 }
 
-export default function RecordSummary({ selectedId, recordType }: RecordSummaryProps) {
+export default function RecordSummary({ selectedId, recordType, bytesLimit }: RecordSummaryProps) {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState(false);
   const [description, setDescription] = useState('');
-  const { mutate: postSummaryRecordDetail } = usePostSummaryRecordDetail();
-  const { data, isPending, isError } = useGetSummaryRecordDetail(selectedId);
-  const { textareaRef, resizeTextarea } = useAutoResizeTextarea(description);
-  const bytesLimit = recordType === 'career' ? 2100 : 1500;
+  const { mutate: postRecordDetail } = usePostRecordDetail();
+  const { data, isPending, isError } = useGetRecordDetail(selectedId);
+  const { textareaRef, resizeTextarea } = useAutoResizeTextarea(description, 150);
 
   useEffect(() => {
     if (data) {
@@ -32,13 +32,10 @@ export default function RecordSummary({ selectedId, recordType }: RecordSummaryP
   }, [data]);
 
   const handleSave = () => {
-    const byteCount = calculateByte(description);
-
-    postSummaryRecordDetail(
+    postRecordDetail(
       {
         recordId: selectedId,
         description,
-        byteCount,
       },
       {
         onSuccess: () => {
@@ -63,42 +60,43 @@ export default function RecordSummary({ selectedId, recordType }: RecordSummaryP
   }
 
   return (
-    <div className="flex flex-col gap-2">
-      <h4 className="font-bold">종합</h4>
-      <Textarea
-        data-testid="summary-input"
-        ref={textareaRef}
-        value={description}
-        onChange={handleDescriptionChange}
-        onInput={resizeTextarea}
-        placeholder="완성본을 적어주세요."
-        className="border-slate-400 p-5 placeholder:text-base placeholder:text-slate-400"
-        style={{
-          lineHeight: 'inherit',
-          fontSize: 'inherit',
-          overflow: 'hidden',
-        }}
-      />
-      <div className="flex justify-end">
-        <span
-          className={`${bytesLimit < calculateByte(description) ? 'text-red-600' : 'text-slate-400'}`}
-        >
-          {calculateByte(description)}
-        </span>
-        <span className="text-slate-400">/{bytesLimit} Bytes</span>
+    <div className="flex w-full flex-col gap-4">
+      <h4 className="text-title-20 text-gray-black">최종</h4>
+      <div className="flex flex-col gap-3 rounded-[20px] border border-gray-2 p-6">
+        <textarea
+          data-testid="summary-input"
+          ref={textareaRef}
+          value={description}
+          onChange={handleDescriptionChange}
+          onInput={resizeTextarea}
+          placeholder="완성본을 적어주세요."
+          className="resize-none border-none bg-transparent p-0 text-body-18-m text-gray-black outline-none placeholder:text-body-18-m placeholder:text-gray-4"
+        />
+        <div className="flex flex-col items-end justify-center self-stretch">
+          <Button
+            color="primary"
+            variant="fill"
+            size="medium"
+            shape="pill"
+            disabled={!description.trim()}
+            onClick={handleSave}
+          >
+            저장
+          </Button>
+        </div>
       </div>
-      <div className="flex justify-end">
-        <button
-          disabled={!description.trim()}
-          className={`rounded-md px-4 pb-1.5 pt-2 text-white ${
-            !description.trim()
-              ? 'cursor-not-allowed bg-slate-400'
-              : 'bg-slate-800 hover:bg-slate-950'
-          }`}
-          onClick={handleSave}
-        >
-          저장
-        </button>
+
+      <div className="flex items-center justify-end self-stretch">
+        <div className="flex items-start gap-[2px]">
+          <span
+            className={`text-body-16-r ${bytesLimit < calculateByte(description) ? 'text-brandRed' : 'text-gray-4'}`}
+          >
+            {calculateByte(description)}
+          </span>
+          <span className="text-body-16-r text-gray-4">/</span>
+          <span className="text-body-16-r text-gray-4">{bytesLimit}</span>
+          <span className="text-body-16-r text-gray-4">Bytes</span>
+        </div>
       </div>
 
       <SaveSummaryRecordModal open={modalOpen} onOpenChange={setModalOpen} />
