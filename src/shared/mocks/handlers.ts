23b0d5bba75d245418458b3aacc1cb5b1355d71e
subsub-e <ab13696802@gmail.com>
2025-09-10 16:32:32 +0900

    EDMT-425 refactor : 레거시 코드인 student-manage 폴더 하위 컴포넌트 파일 제거 및 사용하지 않는 msw 코드, api 코드 제거 및 msw handler 수정

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -19,13 +19,11 @@ import { patchAfterLoginPassword } from '../../domains/profile/mocks/patch-after
 import { patchEmail } from '../../domains/profile/mocks/patch-email';
 import { patchProfile } from '../../domains/profile/mocks/patch-profile';
 import { aiGenerate } from '../../domains/record/mocks/ai-generate';
-import { createRecordDetail } from '../../domains/record/mocks/create-record-detail';
 import { deleteStudents } from '../../domains/record/mocks/delete-students';
 import { getRecordDetail } from '../../domains/record/mocks/get-record-detail';
 import { getRecords } from '../../domains/record/mocks/get-records';
 import { getStudents } from '../../domains/record/mocks/get-students';
 import { getStudentsName } from '../../domains/record/mocks/get-students-name';
-import { patchRecordDetail } from '../../domains/record/mocks/patch-record-detail';
 import { patchStudents } from '../../domains/record/mocks/patch-students';
 import { postRecordDetail } from '../../domains/record/mocks/post-record-detail';
 import { postStudents } from '../../domains/record/mocks/post-students';
@@ -37,8 +35,6 @@ export const handlers = [
   ...getRecords,
   ...getNoticeList,
   ...getNoticeDetail,
-  ...patchRecordDetail,
-  ...createRecordDetail,
   ...signup,
   ...getVerifyEmail,
   ...getStudentsName,
